2f17098a9a7d438b275c8e3b21d13491f6e6e5b3
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2023-02-27 07:41:19 +0300

    JavaOutputFormatting

---
+++ src/JavaOutputFormatting.java
@@ -0,0 +1,18 @@
+//HackerRank
+//https://www.hackerrank.com/challenges/java-output-formatting/problem?isFullScreen=true
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String string = sc.next();
+            int number =sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s", string);
+            System.out.printf("%03d\n" ,number);
+        }
+        System.out.println("================================");
+    }
+}
