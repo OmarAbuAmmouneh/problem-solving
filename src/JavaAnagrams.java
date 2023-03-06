186c96211557cfcc8308237895bdf7c08803b042
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2023-03-06 08:16:46 +0300

    Added problem url

--- src/JavaAnagrams.java
+++ src/JavaAnagrams.java
@@ -1,3 +1,4 @@
+//https://www.hackerrank.com/challenges/java-anagrams/problem?isFullScreen=true
 import java.util.Scanner;
 
 public class JavaAnagrams {
---
+++ src/JavaStringTokens.java
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/java-string-tokens/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+import java.io.*;
+import java.util.*;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+    }
+}
+
