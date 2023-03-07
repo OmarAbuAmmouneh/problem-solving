c5c1dcbf65c98573750856e5a87306e51910202c
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2023-03-07 08:01:07 +0300

    JavaBigDecimal

---
+++ src/JavaBigDecimal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class JavaBigDecimal{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+        for(int i = n - 1; i > 0; i--)
+        {
+            for(int j = 0; j < i ; j++) {
+                if(Double.valueOf(s[j]) < Double.valueOf(s[j + 1])) {
+                    String temp = s[j];
+                    s[j] = s[j + 1];
+                    s[j + 1] = temp;
+                }
+            }
+        }
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
