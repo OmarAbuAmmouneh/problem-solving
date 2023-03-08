e3b1e215426cfcb7d712583217c1a50b4d937d0d
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2023-03-08 22:40:03 +0300

    Modified variables names

--- src/Java2DArray.java
+++ src/Java2DArray.java
@@ -23,10 +23,10 @@ public class Java2DArray {
         int largest = 0;
         for (int i = 0; i < arr.get(0).size() - 2; i++) {
             for(int j = 0; j < arr.get(0).size() - 2; j++) {
-                int firstRow = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2);
-                int secondRow = arr.get(i + 1).get(j + 1);
-                int thirdRow = arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
-                int totalRows = firstRow + secondRow + thirdRow;
+                int firstRowSum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2);
+                int secondRowSum = arr.get(i + 1).get(j + 1);
+                int thirdRowThird = arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                int totalRows = firstRowSum + secondRowSum + thirdRowThird;
                 //The sum might be less than zero
                 if (i == 0 && j == 0) {
                     largest = totalRows;
