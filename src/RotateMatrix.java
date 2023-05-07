f3d8c49216696600955c0d752ab412acacf07a32
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2023-05-07 09:04:24 +0300

    RotateMatrix

---
+++ src/RotateMatrix.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class RotateMatrix {
+
+    public static int[][] RotateMatrix90Degree() {
+        return new  int[2][2];
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+    }
+}
+
