c3cb6c23ce8175bc66be026ee8f09fc4e439e426
OmarAbuAmmouneh <omarabuammouneh@gmail.com>
2023-05-15 18:56:43 +0300

    DeleteLinkedlistDuplicates

---
+++ src/DeleteDuplicates.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+class LinkedListNode {
+    public LinkedListNode next;
+    public int data;
+}
+
+public class DeleteDuplicates {
+
+    private static void deleteDuplicates (LinkedListNode node) {
+        HashSet<Integer> set = new HashSet<>();
+        LinkedListNode previous = null;
+        while (node != null) {
+            if (set.contains(node.data)) {
+                previous.next = node.next;
+            } else {
+                previous = node;
+                set.add(node.data);
+            }
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
